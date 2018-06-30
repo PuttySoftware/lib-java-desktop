312e66cf399f17553b69048548b0c2b1c372d0a9
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-06-30 10:10:33 -0400

    Whoops. Need to export our packages.

--- lib-puttysoftware-desktop/src/module-info.java
+++ lib-puttysoftware-desktop/src/module-info.java
@@ -7,4 +7,14 @@
  */
 module com.puttysoftware.desktop {
     requires transitive java.desktop;
+    exports com.puttysoftware.commondialogs;
+    exports com.puttysoftware.errorlogger;
+    exports com.puttysoftware.fileutils;
+    exports com.puttysoftware.help;
+    exports com.puttysoftware.images;
+    exports com.puttysoftware.images.edges;
+    exports com.puttysoftware.picturepicker;
+    exports com.puttysoftware.scoremanager;
+    exports com.puttysoftware.updater;
+    exports com.puttysoftware.xio;
 }
